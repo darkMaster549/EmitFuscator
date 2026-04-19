3d53a45b748a251b5bc98f194bffa81407058927
Casuke <kylellamas723@gmail.com>
2026-04-19 16:03:06 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ return {
     sep = "Q",
     offset = 90000,
     marker = "LOL!", -- you can change this for your own marker
-    max_passes = 2,
+    max_passes = 2, -- don't change this or Obfuscted code might not work Change it in pipeline maximum is only 2 don't increase it --
     passes = {
         rename    = true,
         strings   = true,
